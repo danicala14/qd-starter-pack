2d4d7f884bd6c98c2f9b195e3fb5835d3f33f87b
Daniele Calabrese <dcala1061@gmail.com>
2025-11-11 19:45:06 +0100

    feat: esercizio5 importato

--- src/random_number.py
+++ src/random_number.py
@@ -4,3 +4,9 @@
 #   Output:
 #   The random number is: 4
 #
+import random
+
+# Genera un numero casuale tra 1 e 10
+numero_casuale = random.randint(1, 10)
+
+print(f"Il numero casuale è: {numero_casuale}")
